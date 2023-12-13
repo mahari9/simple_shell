90839f3963e113ba795d8d0e85b732a25f040f7e
Mahari9 <merytseg77@gmail.com>
2023-12-13 13:54:01 -0800

    Process and handle commands

--- handle_command.c
+++ handle_command.c
@@ -1,6 +1,65 @@
 #include "shell.h"
 
 /**
+ * handle_opera - handles when first operator is either && or ||
+ * @usrin: input line to be tokenized
+ * @opr: logical operator to be handled
+ * Return: 0 on success
+ */
+int handle_opera(char *usrin, char *opr)
+{
+	int result = 0;
+	char *cmnd, *log_and = ma_strstr(usrin, "&&");
+	char *log_or = ma_strstr(usrin, "||");
+
+	cmnd = ma_strtok(usrin, opr);
+	while (cmnd != NULL)
+	{
+		result = ma_parser(cmnd);
+		if (((result == 0) && (ma_strcmp(opr, "||") == 0))
+				|| ((result != 0) && (ma_strcmp(opr, "&&") == 0)))
+			return (result);
+		log_or = ma_strstr(usrin, "||");
+		log_and = ma_strstr(usrin, "&&");
+		if (log_or == NULL || log_and < log_or)
+			opr = "&";
+		else
+			opr = "|";
+		cmnd = ma_strtok(NULL, opr);
+	}
+	return (result);
+}
+
+/**
+ * ma_separat - separate given input into commands
+ * @usrin: input of a line of string
+ * Return: 0 on success
+ */
+int ma_separat(char *usrin)
+{
+	char *log_and, *log_or, *cmnd;
+	int stat = 0;
+
+	if (usrin[0] == ' ' && usrin[ma_strlen(usrin)] == ' ')
+		exit(0);
+	if (!usrin)
+		return (0);
+	cmnd = ma_strtok(usrin, ";");
+	for (; cmnd != NULL;)
+	{
+		log_and = ma_strstr(cmnd, "&&");
+		log_or = ma_strstr(cmnd, "||");
+		if (log_and && (log_or == NULL || log_and < log_or))
+			stat = handle_opera(cmnd, "&&");
+		else if (log_or && (log_and == NULL || log_or < log_and))
+			stat = handle_opera(cmnd, "||");
+		else
+			stat = ma_parser(cmnd);
+		cmnd = ma_strtok(NULL, ";");
+	}
+	return (stat);
+}
+/**
  * ma_parser - splits line into an array of tokens
  * @usrin: input command-line to be splited in arguments
  *
