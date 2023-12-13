b241e35a18134d70bbce2ce71a8b55ce577ad0da
yafiet231 <abieleyasu893@gmail.com>
2023-12-13 11:47:02 -0800

    memo_mang.c

---
+++ memo_mang.c
@@ -0,0 +1,58 @@
+A#include "shell.h"
+
+/**
+ * ma_realloc - reallocates a memory block
+ * @ptr: pointer
+ * @old_size: previous size of the pointer
+ * @new_size: new size of the pointer
+ * Return: Pointer to the reallocated memory
+ */
+void *ma_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *result;
+	char *old_ptr, *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		result = malloc(new_size);
+		if (result == NULL)
+			return (NULL);
+		return (result);
+	}
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (Null);
+	}
+	old_ptr = ptr;
+	result = malloc(sizeof(*old_ptr) * new_size);
+	if (result == NULL)
+		free(ptr);
+	return (NULL);
+
+	new_ptr = result;
+	ma_memcpy(result, ptr, old_size);
+	free(ptr);
+	return (result);
+}
+
+/**
+ * ma_memcpy - copy n bytes of memory from src to dst
+ * @dst: destination pointer
+ * @src: source pointer
+ * @n: size to be copied
+ * Return: pointer to dst
+ */
+char *ma_memcpy(char *dst, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dst[i] = src[i];
+	}
+	return (dst);
+}
