c0ab3b8fb46f9c856e7d01ab561a37de15618da4
yafiet231 <abieleyasu893@gmail.com>
2023-12-15 15:15:08 -0800

    authors

---
+++ AUTHORS.C
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author1
+author1_name= "Mahari Weldegergis"
+author1_email= "
+
+# Author2
+author2= "Abiel Iyasu Abraha Iyasu"
+author2_name= "abieleyasu893@gmail.com"
+
+# Display author information
+echo "Authors:"
+echo "Author 1: $Mahari Weldegergis, Email: $author1_email"
+echo "Author 2: $Abiel Iyasu Abraha Iyasu, Email: $abieleyasu893@gmail.com"
+
